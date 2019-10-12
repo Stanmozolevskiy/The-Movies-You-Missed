747fb14a3627e3cf26f4c1014638233e3644dafb
unknown <stanmozolevskiy@gmail.com>
2019-10-12 14:36:30 -0400

    heruku

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,21 +1,29 @@
 import http from "./http";
+require("dotenv/config");
 
+let baseURL = "";
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3900";
+} else {
+  baseURL = window.location.origin;
+}
+console.log(baseURL);
 export function regester(user) {
-  return http.post(`${window.location.origin}/api/users`, {
+  return http.post(`${baseURL}/api/users`, {
     name: user.name,
     email: user.email,
     password: user.password
   });
 }
 export function singnIn(user) {
-  return http.post(`${window.location.origin}/api/auth`, {
+  return http.post(`${baseURL}/api/auth`, {
     email: user.email,
     password: user.password
   });
 }
 
 export function regesterWithAccount(user) {
-  return http.post(`${window.location.origin}/api/userswithaccount`, {
+  return http.post(`${baseURL}/api/userswithaccount`, {
     confirmed: user.confirmed,
     name: user.name,
     email: user.email,
@@ -24,7 +32,7 @@ export function regesterWithAccount(user) {
 }
 
 export function singnInWithAccount(user) {
-  return http.post(`${window.location.origin}/api/auth`, {
+  return http.post(`${baseURL}/api/auth`, {
     email: user.email,
     password: user.password
   });
