00460a8b7cf73c11ad4631ec01edfa36a1beb12d
unknown <stanmozolevskiy@gmail.com>
2019-11-29 10:26:01 -0500

    ssl

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/.well-known/acme-challenge/m9PWs7xgAMaueBP8L-gZ0yXWY9XV6EmDqm5K1p7t56M
   res.send('m9PWs7xgAMaueBP8L-gZ0yXWY9XV6EmDqm5K1p7t56M.FrqwX3b9fL_dJZKKNsvCb3eP_fkT2V7ueeyjyU9XN_w')
 })
 
+app.get('/', function (req, res) {
+  res.send('m9PWs7xgAMaueBP8L-gZ0yXWY9XV6EmDqm5K1p7t56M.FrqwX3b9fL_dJZKKNsvCb3eP_fkT2V7ueeyjyU9XN_w')
+})
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
