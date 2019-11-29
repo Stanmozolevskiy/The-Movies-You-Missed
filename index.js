97a4ff2ad52403dc52a6ad6aafec48fbe35087c5
unknown <stanmozolevskiy@gmail.com>
2019-11-29 10:29:22 -0500

    ssl

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3000;
 
 // app.use(express.json());
 
-app.get('/.well-known/acme-challenge/m9PWs7xgAMaueBP8L-gZ0yXWY9XV6EmDqm5K1p7t56M', function (req, res) {
-  res.send('m9PWs7xgAMaueBP8L-gZ0yXWY9XV6EmDqm5K1p7t56M.FrqwX3b9fL_dJZKKNsvCb3eP_fkT2V7ueeyjyU9XN_w')
+app.get('/.well-known/acme-challenge/YkaKwcciHu0uluGG0g8lznZRBz7fXLp7q4w_DRb3_LE', function (req, res) {
+  res.send('YkaKwcciHu0uluGG0g8lznZRBz7fXLp7q4w_DRb3_LE.FrqwX3b9fL_dJZKKNsvCb3eP_fkT2V7ueeyjyU9XN_w')
 })
 
 app.get('/', function (req, res) {
-  res.send('m9PWs7xgAMaueBP8L-gZ0yXWY9XV6EmDqm5K1p7t56M.FrqwX3b9fL_dJZKKNsvCb3eP_fkT2V7ueeyjyU9XN_w')
+  res.send('YkaKwcciHu0uluGG0g8lznZRBz7fXLp7q4w_DRb3_LE.FrqwX3b9fL_dJZKKNsvCb3eP_fkT2V7ueeyjyU9XN_w')
 })
 
 app.listen(port, () => {
