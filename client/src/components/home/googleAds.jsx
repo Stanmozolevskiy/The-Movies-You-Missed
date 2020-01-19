4b72f4b25c71a9bf94b0a177d03d42e0a28954c2
unknown <stanmozolevskiy@gmail.com>
2020-01-19 16:20:34 -0500

    google adds

--- client/src/components/home/googleAds.jsx
+++ client/src/components/home/googleAds.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import AdSense from 'react-adsense';
+import React, { Component } from "react";
+import AdSense from "react-adsense";
 
 class GoogleAds extends Component {
-    state = {}
-    render() {
-        return (<div>
-
-            <AdSense.Google
-                client='ca-pub-9427041283177019'
-                slot='1719342757'
-                style={{ display: 'block' }}
-                format='auto'
-                responsive='true'
-                layoutKey='-gw-1+2a-9x+5c'
-            />
-        </div>);
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <AdSense.Google
+          client="ca-pub-9427041283177019"
+          slot="1719342757"
+          style={{ display: "block", float: "inherit" }}
+          format="auto"
+          responsive="true"
+          layoutKey="-gw-1+2a-9x+5c"
+        />
+      </div>
+    );
+  }
 }
 
 export default GoogleAds;
