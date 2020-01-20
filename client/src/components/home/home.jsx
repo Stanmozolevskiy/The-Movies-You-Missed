c28f77a296668884c83bf7df60f8fda995364f92
Stanislav Mozolevskiy <smozolevskiy@b2wsoftware.com>
2020-01-20 11:55:09 -0500

    adsence

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -111,10 +111,10 @@ class Home extends Component {
               <AdSense.Google
                 client="ca-pub-9427041283177019"
                 slot="1719342757"
-                style={{ display: "block" }}
-                format="auto"
-                responsive="true"
-                layoutKey="-gw-1+2a-9x+5c"
+                // style={{ display: "block" }}
+                // format="auto"
+                // responsive="true"
+                // layoutKey="-gw-1+2a-9x+5c"
               />
             </div>
 
