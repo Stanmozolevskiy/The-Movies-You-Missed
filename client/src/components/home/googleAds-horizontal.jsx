064fa5685d95c8f7701954e052bfe797c26f491e
Stanislav Mozolevskiy <smozolevskiy@b2wsoftware.com>
2020-01-21 13:04:40 -0500

    adds

--- client/src/components/home/googleAds-horizontal.jsx
+++ client/src/components/home/googleAds-horizontal.jsx
@@ -10,7 +10,7 @@ class GoogleAdsHorizontal extends Component {
       <div className='ad'>
         <ins
           className="adsbygoogle"
-          style={{ display: "block", textAlign: "center", margin: "auto", width: "auto", height: "auto" }}
+          style={{ display: "block", textAlign: "center", width: "100%", height: "auto", padding: "20%" }}
           data-ad-client="ca-pub-9427041283177019"
           data-ad-slot="6060383760"
           data-ad-format="auto"
