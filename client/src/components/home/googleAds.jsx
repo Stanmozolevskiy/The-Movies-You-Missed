945df298caca713c14d3ec2b6193346918923f8f
unknown <stanmozolevskiy@gmail.com>
2020-01-22 09:10:38 -0500

    adds

--- client/src/components/home/googleAds.jsx
+++ client/src/components/home/googleAds.jsx
@@ -7,15 +7,13 @@ class GoogleAds extends Component {
 
   render() {
     return (
-      <div className='ad' style={{ padding: "20%" }}>
+      <div className="ad" style={{ padding: "20%" }}>
         <ins
-          className='adsbygoogle '
-          style={{ display: "block", textAlign: "center", width: "100%", height: "auto" }}
-          data-ad-client='ca-pub-9427041283177019'
-          data-ad-slot='1719342757'
-          // data-ad-format='auto'
-          responsive="true"
-        />
+          className="adsbygoogle"
+          style={{ display: "inline-block", width: "300px", height: "200px" }}
+          data-ad-client="ca-pub-9427041283177019"
+          data-ad-slot="6060383760"
+        ></ins>
       </div>
     );
   }
