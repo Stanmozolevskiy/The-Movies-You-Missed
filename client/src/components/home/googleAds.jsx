8f73a3cf8a983ee9b12684ad04fb8c7959f7deef
Stanislav Mozolevskiy <42652969+Stanmozolevskiy@users.noreply.github.com>
2020-01-29 17:18:41 -0500

    Update googleAds.jsx

--- client/src/components/home/googleAds.jsx
+++ client/src/components/home/googleAds.jsx
@@ -9,7 +9,7 @@ class GoogleAds extends Component {
     return (
       <div>
         <ins
-          className="adsbygoogle adContent"
+          style="display:inline-block;width:300px;height:600px"
           data-ad-client="ca-pub-9427041283177019"
           data-ad-slot="6060383760"
         ></ins>
