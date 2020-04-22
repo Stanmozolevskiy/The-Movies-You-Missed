f951a04598e7b4b3c1db72cbd563df1b9bb4cc45
stanmozolevskiy <stanmozolevskiy@gmail.com>
2020-04-22 18:21:06 -0400

    css devided

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
+import "./Footer.css"
+import "./mobile-responsive.css"
 import App from './App';
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
